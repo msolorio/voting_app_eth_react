1b9ffa05c914d63e7cc5e5f53f623ee6a66e7b7e
msolorio <msolorio.github@gmail.com>
2021-10-07 21:37:00 -0700

    Adds functionality to create and get polls

---
+++ contracts/App.sol
@@ -0,0 +1,49 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract App {
+
+  struct Poll {
+    string title;
+    string description;
+  }
+
+  Poll[] private pollsList;
+
+  mapping(address => Poll) addrToPoll;
+
+  function createPoll(string memory _title, string memory _description) public {
+    Poll memory newPoll = Poll({
+      title: _title,
+      description: _description
+    });
+
+    pollsList.push(newPoll);
+  }
+
+  function getPolls() public view returns (Poll[] memory) {
+    console.log('Retrieving all polls');
+
+    return pollsList;
+  }
+}
+
+// contract Greeter {
+//     string private greeting;
+
+//     constructor(string memory _greeting) {
+//         console.log("Deploying a Greeter with greeting:", _greeting);
+//         greeting = _greeting;
+//     }
+
+//     function greet() public view returns (string memory) {
+//         return greeting;
+//     }
+
+//     function setGreeting(string memory _greeting) public {
+//         console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+//         greeting = _greeting;
+//     }
+// }
--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import AppContract from './artifacts/contracts/App.sol/App.json';
 import './App.css';
 
+const appContractAddr = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
 function App() {
+  const [state, setState] = useState({
+    title: '',
+    description: ''
+  });
+
+  const handleInputChange = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    })
+  }
+  
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    console.log('submit form');
+
+    if (!state.title || !state.description) return;
+    if (!window.ethereum) return;
+    
+    try {
+      await requestAccount();
+      
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(appContractAddr, AppContract.abi, signer);
+      
+      const transaction = await contract.createPoll(state.title, state.description);
+      await transaction.wait();
+
+      console.log('created poll');
+    } catch(err) {
+      console.log('Error creating poll ==>', err);
+    }
+  }
+  
+  const getPolls = async () => {
+    if (!window.ethereum) return;
+    
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(appContractAddr, AppContract.abi, provider);
+
+    try {
+      const polls = await contract.getPolls();
+      console.log('All Polls ==>', polls);
+
+    } catch (err) {
+      console.log('Error getting polls ==>', err);
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create a Poll</h1>
+
+    <form onSubmit={handleFormSubmit}>
+      <div>
+        <label htmlFor="title">Title: </label>
+        <input 
+          type="text" 
+          name="title" 
+          value={state.title}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="description">Description: </label>
+        <textarea
+          type="text" 
+          name="description" 
+          value={state.description}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <input type="submit" value="Create" />
+    </form>
+
+      <input
+        type="button"
+        value="Get Polls" 
+        onClick={getPolls}
+      />
     </div>
   );
 }
