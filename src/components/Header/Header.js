608f9e4b4ec8349c821f436b82cd6654a0bcb8b4
msolorio <msolorio.github@gmail.com>
2022-04-01 17:11:13 -0700

    Updates app title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <h1 className="header-title">Voting App</h1>
+      <h1 className="header-title">Voterocity</h1>
       <nav className="header-nav">
         <ul className="header-ul">
           <li className="header-li"><Link className="header-link" to="/polls/new">Create</Link></li>
